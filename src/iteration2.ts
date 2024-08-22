6ca51e2909f9507b3324d718e85e0c1e458ffaf9
Simon Schubert <simonlouis.schubert@gmail.com>
2024-08-22 13:52:58 +0200

    Completed lab

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -2,4 +2,23 @@
 // Iteration 2 | Functions
 //
 
+const calcMultiplication = (a: number, b: number): number => {
+  return a * b;
+};
 
+const isEven = (n: number): boolean => {
+  if (n % 2 === 0) return true;
+  else return false;
+};
+
+const calcArrayAverage = (numberArr: number[]): string | number => {
+  if (!numberArr.length)
+    return "Please provide an array with at least one element.";
+  else {
+    const sum = numberArr.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+    const avg = sum / numberArr.length;
+    return avg;
+  }
+};
