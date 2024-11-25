14489236b84234bb622f5b39bbdaaaeb9dc136db
Skyteks <Schattenpalkia@googlemail.com>
2024-11-25 16:14:30 +0100

    done

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -1,5 +1,19 @@
 //
 // Iteration 2 | Functions
 //
+function calcMultiplication(a: number, b: number): number {
+    return a * b;
+}
 
+function isEven(n:number):boolean {
+    return n % 2 === 0;
+}
 
+function calcArrayAverage(numbersArr:number[]):string | number {
+    if (numbersArr.length == 0) {
+        return "Please provide an array with at least one element";
+    }
+    const sum = numbersArr.reduce((acc, curr) => acc + curr, 0);
+    const avg = (sum / numbersArr.length);
+    return avg;
+}
--- src/iteration5.ts
+++ src/iteration5.ts
@@ -1,5 +1,34 @@
 //
 // Bonus: Iteration 5 | Interfaces
 //
+interface MediaItem {
+    title: string;
+    year:number;
+    genres: string[];
+    rating: number | undefined;
+}
 
+interface Movie extends MediaItem {
+    title: string;
+    year: number;
+    genres: string[];
+    rating: number;
+    director: string;
+    durationInMinutes: number;
+}
 
+enum AlbumTypes {
+    StudioAlbum,
+    LiveAlbum,
+    Soundtrack,
+    Other,
+}
+interface MusicAlbum extends MediaItem {
+    artist: string;
+    durationInMinutes: number;
+    albumType: AlbumTypes;
+}
+
+function getYearsSinceRelease(mediaItem: MediaItem) :number{
+    return new Date().getFullYear() - mediaItem.year;
+}
