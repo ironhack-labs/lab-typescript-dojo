2cfb2faf11d0a506f1f374d0edc20767d8473a35
Coco Greig <cocogreig@hotmail.co.uk>
2024-11-25 18:40:00 +0100

    Lab solved

--- src/iteration4.ts
+++ src/iteration4.ts
@@ -2,4 +2,47 @@
 // Iteration 4 | Type aliases
 //
 
+type Book = {
+  title: string;
+  year: number;
+  genres: string[];
+  rating: number;
+  author: string;
+  numberOfPages?: number;
+};
 
+type Movie = {
+  title: string;
+  year: number;
+  genres: string[];
+  rating: number;
+  director: string;
+  durationInMinutes: number;
+};
+
+type MusicAlbum = {
+  title: string;
+  year: number;
+  genres: string[];
+  rating: number;
+  numberOfTracks: number;
+  durationInMinutes: number;
+  albumType: "Studio Album" | "Live Album" | "Soundtrack" | "Other";
+};
+
+const result1: Movie = {
+  title: "lalaland",
+  year: 2015,
+  genres: ["drama", "mystery"],
+  rating: 8,
+  director: "M Night",
+  durationInMinutes: 305,
+};
+
+function getYearsSinceRelease(mediaItem: Book | Movie | MusicAlbum): number {
+  const currentYear = new Date().getFullYear();
+  const difference = currentYear - mediaItem.year;
+  return difference;
+}
+
+console.log(getYearsSinceRelease(result1));
