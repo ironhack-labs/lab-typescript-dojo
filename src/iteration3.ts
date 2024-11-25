f6828c664177d5c1d83938ac0239d295dae9ca84
Konstantinos Gerogiannis <kostger2@gmail.com>
2024-11-25 14:56:27 +0100

    done

--- src/iteration3.ts
+++ src/iteration3.ts
@@ -2,4 +2,20 @@
 // Iteration 3 | Classes
 //
 
+class BankAcount {
+  balance: number = 0;
+  accountHolder: string;
 
+  constructor(accountHolder: string) {
+    this.accountHolder = accountHolder;
+  }
+  getBalance(): number {
+    return this.balance;
+  }
+  deposit(amount: number): void {
+    this.balance += amount;
+  }
+  withdraw(amount: number): void {
+    this.balance -= amount;
+  }
+}
