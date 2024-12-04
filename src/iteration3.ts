ab70eab370db7c548815eb01a55c9357618d4a5e
nathidaum <daumnathi@gmail.com>
2024-12-04 16:47:38 +0100

    Finished lab

--- src/iteration3.ts
+++ src/iteration3.ts
@@ -2,4 +2,37 @@
 // Iteration 3 | Classes
 //
 
+// 3.1
 
+class Bankaccount {
+  balance: number = 0;
+  accountHolder: string;
+
+  constructor(accountHolder: string) {
+    this.accountHolder = accountHolder;
+  }
+
+  getBalance(): number {
+    return this.balance;
+  }
+
+  deposit(amount: number): string | number {
+    if (amount <= 0) {
+      return "Please provide a valid amount";
+    } else {
+      this.balance += amount;
+      return this.balance;
+    }
+  }
+
+  withdraw(amount: number): string | number {
+    if (amount <= 0) {
+      return "Please provide a valid amount";
+    } else if (amount > this.balance) {
+      return "Insufficient funds";
+    } else {
+      this.balance -= amount;
+      return this.balance;
+    }
+  }
+}
--- src/iteration5.ts
+++ src/iteration5.ts
@@ -2,4 +2,35 @@
 // Bonus: Iteration 5 | Interfaces
 //
 
+// 5.1
+interface MediaItem {
+  title: string;
+  year: number;
+  genres: string[];
+  rating: number;
+}
 
+// 5.2
+
+interface MediaItem {
+  author: string;
+  numberOfPages?: number;
+}
+
+interface Movie extends MediaItem {
+  director: string;
+  durationInMinutes?: number;
+}
+
+interface MusicAlbum extends MediaItem {
+  artist: string;
+  numberOfTracks?: number;
+  durationInMinutes?: number;
+  albumType: "Studio Album" | "Live Album" | "Soundtrack " | "Other";
+}
+
+// 5.3
+const getYearsSinceRelease = (mediaItem: MediaItem): number => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - mediaItem.year;
+};
