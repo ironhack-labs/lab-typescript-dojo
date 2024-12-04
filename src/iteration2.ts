ed5585e106ce62b89beb2844dff063d0c6654d77
Batuhan Yildirim <bthn.yildirim@gmail.com>
2024-12-04 20:02:39 +0100

    completed lab

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -2,4 +2,28 @@
 // Iteration 2 | Functions
 //
 
+function calcMultiplication(a: number, b: number): number {
+  return a * b;
+}
 
+function isEven(n: number): boolean {
+  if (n % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function calcArrayAverage(numbersArr: number[]): number | string {
+  if (numbersArr.length === 0) {
+    return "Please provide an array with at least one element";
+  }
+
+  const total = numbersArr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  });
+
+  const average = total / numbersArr.length;
+
+  return average;
+}
--- src/iteration3.ts
+++ src/iteration3.ts
@@ -1,5 +1,36 @@
 //
 // Iteration 3 | Classes
 //
+class BankAccount {
+  balance: number = 0;
+  accountHolder: string;
 
+  constructor(accountHolder: string) {
+    this.accountHolder = accountHolder;
+  }
 
+  getBalance(): number {
+    return this.balance;
+  }
+
+  deposit(amount: number): number | string {
+    if (amount <= 0) {
+      return `Please provide a valid amount`;
+    }
+
+    this.balance += amount;
+    return this.balance;
+  }
+
+  withdraw(amount: number): number | string {
+    if (amount <= 0) {
+      return `Please provide a valid amount`;
+    }
+    if (amount > this.balance) {
+      return `Insufficient funds`;
+    }
+
+    this.balance -= amount;
+    return this.balance;
+  }
+}
