c200248d1d5282158788ca5931c7c0cf9326b22b
Geethu Pandath Ramesh <geethuramesh2@gmail.com>
2024-12-04 15:46:09 +0100

    done

--- src/iteration3.ts
+++ src/iteration3.ts
@@ -2,4 +2,67 @@
 // Iteration 3 | Classes
 //
 
+// class BankAccount {
+//     balance: number = 0;
+//     accountHolder: string;
 
+//     constructor(accountHolder: string){
+//         this.accountHolder = accountHolder;
+//     }
+
+//     getBalance(): number{
+//         return this.balance;
+//     }
+// }  
+// const myAccount = new BankAccount("Alice");
+// console.log(`Account Holder: ${myAccount.accountHolder}`);
+// console.log(`Initial Balance: ${myAccount.getBalance()}`);
+
+class BankAccount {
+    balance: number = 0;
+    accountHolder: string;
+
+    constructor(accountHolder: string) {
+        this.accountHolder = accountHolder;
+    }
+
+    getBalance(): number {
+        return this.balance;
+    }
+
+    deposit(amount: number): number | string {
+        if (amount <= 0) {
+            return `Please provide a valid amount`;
+        }
+
+        this.balance += amount;
+        return this.balance;
+    }
+
+    withdraw(amount: number): number | string {
+        if (amount <= 0) {
+            return `Please provide a valid amount`;
+        }
+        if (amount > this.balance) {
+            return `Insufficient funds`;
+        }
+
+        this.balance -= amount;
+        return this.balance;
+    }
+}
+
+
+const myAccount = new BankAccount("Alice");
+
+// // Deposit money
+// console.log(myAccount.deposit(100)); 
+// console.log(myAccount.deposit(-50));
+
+// // Check balance
+// console.log(myAccount.getBalance()); 
+
+// // Withdraw money
+// console.log(myAccount.withdraw(50)); 
+// console.log(myAccount.withdraw(100)); 
+// console.log(myAccount.withdraw(-20)); 
--- src/iteration4.ts
+++ src/iteration4.ts
@@ -2,4 +2,62 @@
 // Iteration 4 | Type aliases
 //
 
+type Book = {
+    title: string;
+    year: number;
+    genres: string[];
+    rating?: number;
+    author: string;
+    numberOfPages?: number;
+}  
+type Movie = {
+    title: string;
+    year: number;
+    genres: string[];
+    rating?: number;
+    director: string;
+    durationInMinutes?: number;
+}  
+type MusicAlbum = {
+    title: string;
+    year: number;
+    genres: string[];
+    rating?: number;
+    artist: string;
+    numberOfTracks?: number;
+    durationInMinutes?: number;
+    albumType: 'Studio Album' | 'Live Album' | 'Soundtrack' | 'Other';
+}  
+function getYearsSinceRelease(mediaItem: Book | Movie | MusicAlbum): number {
+    const currentYear = new Date().getFullYear();
+    const difference = currentYear - mediaItem.year;
+    return difference;
+} 
 
+// const myBook: Book = {
+//     title: "abc",
+//     year: 1960,
+//     genres: ["Drama"],
+//     author: "Hffg",
+//     numberOfPages: 281,
+// };
+
+// const myMovie: Movie = {
+//     title: "tfvggg",
+//     year: 2010,
+//     genres: ["Thriller"],
+//     director: "Cfg",
+//     durationInMinutes: 148,
+// };
+
+// const myAlbum: MusicAlbum = {
+//     title: "hjggk",
+//     year: 1969,
+//     genres: ["Rock"],
+//     artist: "The Beatles",
+//     albumType: "Studio Album",
+//     numberOfTracks: 17,
+// };
+// console.log(`Years since release of "${myBook.title}": ${getYearsSinceRelease(myBook)}`);
+// console.log(`Years since release of "${myMovie.title}": ${getYearsSinceRelease(myMovie)}`);
+// console.log(`Years since release of "${myAlbum.title}": ${getYearsSinceRelease(myAlbum)}`);
