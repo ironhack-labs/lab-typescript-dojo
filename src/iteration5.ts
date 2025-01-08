8cf10d1d12be00e786dd2e22b45e6f2b9636d88d
Bruno Frasan <brunofrancco@gmail.com>
2025-01-08 13:06:30 +0000

    Completed lab

--- src/iteration5.ts
+++ src/iteration5.ts
@@ -2,4 +2,31 @@
 // Bonus: Iteration 5 | Interfaces
 //
 
+interface MediaItem {
+	title: string
+	year: number
+	genres: Array<string>
+	rating?: number
+}
 
+interface Book extends MediaItem {
+	author: string
+	numberOfPages?: number
+}
+
+interface Movie extends MediaItem {
+	director: string
+	durationInMinutes?: number
+}
+
+interface MusicAlbum extends MediaItem {
+	artist: string
+	numberOfTracks?: number
+	durationInMinutes?: number
+	albumType: 'Studio Album' | 'Live Album' | 'Soundtrack' | 'Other'
+}
+
+function getYearsSinceRelease(mediaItem: MediaItem): number {
+	const yearToday: number = new Date().getFullYear()
+	return yearToday - mediaItem.year
+}
